71b36c95c7ac8007715ee69e5ec15b873230f26e
Thays Peixoto <thabysilva12@gmail.com>
2025-10-10 12:48:26 -0300

    reestruturação de arquivo

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import { TopBar } from './components/topbar';
+import { TopBanner } from './components/topbanner';
+import { TopPosts } from './components/topposts';
+import { Featured } from './components/featured';
+import { Footer } from './components/footer';
+
+function App() {
+  return (
+    <>
+    <TopBar/>
+    <TopBanner/>
+    <TopPosts/>
+    <Featured/>
+    <Footer/>
+    
+    </>
+  );
+}
+
+export default App
