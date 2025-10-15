53ced555fe7cfd33a09f7ce7595b0c67faaae27a
Thays Peixoto <thabysilva12@gmail.com>
2025-10-15 16:24:19 -0300

    criação de useState para menu

--- src/components/topbar/index.jsx
+++ src/components/topbar/index.jsx
@@ -1,17 +1,53 @@
+import { useState } from "react";
 import "./style.css";
+// import WorksIcon from "@mui";
+// import blogIcons from "";
+// import contactIcons from "";
+import Drawer from "@mui/material/Drawer";
+
 
 
 export function TopBar(){
+    const  [openMenu, setOpenMenu] = useState(false);
+    const menuOptions = [
+            { text: "Works", href: "#" },
+            { text: "Blogs", href: "#" },
+            { text: "Contact", href: "#" },
+        ];
+
+
+
     return(
       
-        <nav className="topbar-container">     
-        <a href="#">Works</a>
-        <a href="#">Blogs</a>
-        <a href="#">Contact</a>
-        <button className="menu-hamburguer" aria-label="Abrir Menu">
-        &#9776; </button>
-        </nav>
+       <nav className="topbar-container">
+      {menuOptions.map((option) => (
+        <a key={option.text} href={option.href}>
+          {option.text}
+        </a>
+      ))}
 
+
+        <div className="container-hamburguer">
+        <button className="menu-hamburguer" onClick={() => setOpenMenu(true)} aria-label="Abrir Menu">
+        &#9776; </button>
+        </div>
+
+         <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
+        <div className="drawer-content">
+        <button className="close-button" onClick={() => setOpenMenu(false)}>
+        ✕
+        </button>
+       
+          {menuOptions.map((option) => (
+            <a key={option.text} href={option.href}>
+              {option.text}
+            </a>
+          ))}
+        </div>
+      </Drawer>
+
+        </nav>
+        
     );
 }
 
